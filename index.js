337116689b832759055d342fbffc92c023015443
Scott Voyles <zeitchef@gmail.com>
2019-06-08 19:28:57 +0200

    Restructuring index.js

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const axios = require('axios')
 const colors = require('colors')
 
-const url = 'https://favqs.com/api/qotd'
+module.exports = async () => {
+  const url = 'https://favqs.com/api/qotd'
 
-async function getQuote() {
   try {
     const response = await axios.get(url)
+
     const quote = response.data.quote.body
     const author = response.data.quote.author
+    const wrappedText = quote.replace(
+      /(?![^\n]{1,60}$)([^\n]{1,60})\s/g,
+      '$1\n'
+    )
 
-    const wrapped = quote.replace(/(?![^\n]{1,60}$)([^\n]{1,60})\s/g, '$1\n')
-
-    console.log(`"${wrapped}"\n`)
+    console.log(`"${wrappedText}"\n`)
     console.log(`- ${author}`.green)
   } catch (error) {
     console.error('ERROR'.red.inverse, `${error.message}`)
   }
 }
-
-getQuote()
